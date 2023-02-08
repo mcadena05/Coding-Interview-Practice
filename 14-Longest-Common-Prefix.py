1d30a3144a3278758e6712a6564b7f4fd3ae14a8
mcadena05 <cadenamarlynn@gmail.com>
2023-02-08 12:35:06 -0700

    first commit

---
+++ 14-Longest-Common-Prefix.py
@@ -0,0 +1,26 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+ 
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ''
+        shortest = min(strs, key = len)
+
+        for i, char in enumerate(shortest):
+            for word in strs:
+                if word[i] != char:
+                    return shortest[:i]
+        return shortest 
