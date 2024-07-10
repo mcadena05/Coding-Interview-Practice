cfd3a1212b7d50f4363ebb9879d9764dac032e15
mcadena05 <cadenamarlynn@gmail.com>
2024-07-10 17:57:57 -0600

    remove element

--- 1071-Greatest-Common-Divisor-Strings.py
+++ 1071-Greatest-Common-Divisor-Strings.py
@@ -1,12 +1,12 @@
-len1, len2 = len(str1), len(str2)
+# len1, len2 = len(str1), len(str2)
 
-    def isDivisor(1):
-        if len1 % 1 or len2 % 1:
-            return False
-        f1, f2 =len1 // 1, len2 // 2
-        return str1[:1] * f1 == str1 and str1[:1] *f2 == str2
+#     def isDivisor(1):
+        # if len1 % 1 or len2 % 1:
+        #     return False
+        # f1, f2 =len1 // 1, len2 // 2
+        # return str1[:1] * f1 == str1 and str1[:1] *f2 == str2
 
-    for l in range(min(len1, len2), 0, -1):
-        if isDivisor(1):
-            return str1[:1]
-    return ''
+        # for l in range(min(len1, len2), 0, -1):
+        #     if isDivisor(1):
+        #         return str1[:1]
+        # return ''
