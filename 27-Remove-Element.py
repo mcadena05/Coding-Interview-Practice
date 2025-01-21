16e3176bd82c6c806c0db4ee1361caa5e8cd2105
mcadena05 <cadenamarlynn@gmail.com>
2025-01-21 12:56:59 -0700

    Practice 1-21-25

--- 27-Remove-Element.py
+++ 27-Remove-Element.py
@@ -1,15 +1,15 @@
 def removeElement(self, nums: List[int], val: int) -> int:
     n = 0
-            swap = 0
+    swap = 0
 
-            for i, v in enumerate(nums):
-                if v != val:
-                    # swap with right
-                    t = nums[swap]
-                    nums[swap] = nums[i]
-                    nums[i] = t
+    for i, v in enumerate(nums):
+        if v != val:
+            # swap with right
+            t = nums[swap]
+            nums[swap] = nums[i]
+            nums[i] = t
 
-                    swap += 1
-                    n += 1
+            swap += 1
+            n += 1
         
-            return n
+        return n
