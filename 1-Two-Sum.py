7289764f40bc467af2bbac844dff1f8e3be6620a
mcadena05 <cadenamarlynn@gmail.com>
2025-02-04 10:06:13 -0700

    Practice 2-3-25 Afternoon

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -26,3 +26,9 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+                
+
+for i in range(len(nums)):
+            for j in range(len(nums)-1, -1, -1):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
